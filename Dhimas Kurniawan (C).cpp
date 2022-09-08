0f2480efbfdda7376df79885b16e7bc3f2f41760
DhimasKurniawan993 <112592752+DhimasKurniawan993@users.noreply.github.com>
2022-09-08 14:08:27 +0700

    Add files via upload

---
+++ Dhimas Kurniawan (C).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int x , y;
+	cin >> x >> y;
+	cout << x + y << "\n";
+	cout << x - y << "\n";
+	cout << x * y << "\n";
+	cout << x / y << "\n";
+	cout << x % y << "\n";
+	return 0;
+}
